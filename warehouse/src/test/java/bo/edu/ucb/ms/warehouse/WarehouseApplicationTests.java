0602164ddafc03ba85cc95712d810bc103be8e7b
Gabriel Chávez <gchavezmardonez@gmail.com>
2025-09-03 03:07:12 -0400

    Adds warehouse base module

---
+++ warehouse/src/test/java/bo/edu/ucb/ms/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ms.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
